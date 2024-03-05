f7aaf972fb2355bcaa7aff223249ebe99245a6dc
Colin Spiridonov <colinspiri@gmail.com>
2024-03-04 20:00:22 -0800

    enemy spawner works

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections.Generic;
 using ScriptableObjectArchitecture;
+using Scripts.Player;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private bool spawnSwarmEnemy;
     [SerializeField]
     private GameObject swarmEnemyParentPF;
 
+    [SerializeField] private bool spawnMeleeEnemy;
     [SerializeField]
-    private GameObject meleeEnemyParentPF;
+    private GameObject meleeEnemyPF;
+
+    [SerializeField] private bool spawnFlyingEnemy;
+    [SerializeField]
+    private GameObject flyingEnemyPF;
 
     [SerializeField]
     private List<Transform> spawnLocations;
@@ -20,11 +29,50 @@ public class EnemySpawner : MonoBehaviour
     private int maxEnemyCount;
 
     [SerializeField]
-    private float spawnCooldown;
+    private float spawnTime;
+
+    private float _spawnTimer;
 
     // Start is called before the first frame update
-    void Start() { }
+    void Start() {
+        _spawnTimer = spawnTime;
+    }
 
     // Update is called once per frame
-    void Update() { }
+    void Update() {
+        if (_spawnTimer <= 0) {
+            if (enemyCollection.Count < maxEnemyCount) {
+                SpawnEnemy();
+                _spawnTimer = spawnTime;   
+            }
+        }
+        else _spawnTimer -= Time.deltaTime;
+    }
+
+    private void SpawnEnemy() {
+        // choose transform farthest from player
+        float maxDistance = 0;
+        var farthestSpawnLocation = transform;
+        foreach (var location in spawnLocations) {
+            var distance = Vector3.Distance(PlayerCharacterController.Instance.transform.position, location.transform.position);
+            if (distance > maxDistance) {
+                maxDistance = distance;
+                farthestSpawnLocation = location;
+            }
+        }
+        
+        // choose random enemy
+        var enemiesToSpawn = new List<GameObject>();
+        if(spawnMeleeEnemy) enemiesToSpawn.Add(meleeEnemyPF);
+        if(spawnFlyingEnemy) enemiesToSpawn.Add(flyingEnemyPF);
+        if(spawnSwarmEnemy) enemiesToSpawn.Add(swarmEnemyParentPF);
+        
+        var randomIndex = Random.Range(0, enemiesToSpawn.Count);
+        var newEnemyPrefab = enemiesToSpawn[randomIndex];
+        
+        Debug.Log("spawning " + newEnemyPrefab.name);
+
+        // spawn enemy 
+        Instantiate(newEnemyPrefab, farthestSpawnLocation.position, Quaternion.identity);
+    }
 }
