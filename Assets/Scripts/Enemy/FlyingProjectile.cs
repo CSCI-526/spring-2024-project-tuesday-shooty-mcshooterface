aae43f9e2b2884fe7f2af88f4726b768b7040783
admolani <admolani@gmail.com>
2024-03-05 12:32:48 -0800

    Added Flying Projectile

---
+++ Assets/Scripts/Enemy/FlyingProjectile.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Scripts.Game;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+[Enemy]
+public class FlyingProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.cyan;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null && (other.gameObject.layer == LayerMask.NameToLayer("Player")))
+        {
+            Debug.Log("Hit by Flying Projectile!");
+            HealthComponent hp = other.gameObject.GetComponent<HealthComponent>();
+            if (hp != null)
+            {
+                DamageInfo d = new DamageInfo(1, BulletColor.Empty, GetType().Name);
+                hp.TakeDamage(d);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
